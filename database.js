3b7e4cb5f2678e33487e9afd775400f4e2e79dcf
SolindekDev <83218819+SolindekDev@users.noreply.github.com>
2022-05-22 11:06:35 +0200

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 class Database {
   constructor() {
-    const uri = "mongodb://localhost:27017/developerchat";
+//     const uri = "mongodb://localhost:27017/developerchat";
+    const uri = process.env.DATABASE_URI;
 
     mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 
